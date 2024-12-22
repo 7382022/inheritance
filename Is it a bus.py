6004f062c3618027156a31478a97baed01755b3f
7382022 <amreenrida2@gmail.com>
2024-12-22 10:10:46 -0500

    Vechile

---
+++ Is it a bus.py
@@ -0,0 +1,15 @@
+class Vechile:
+
+    def __init__(self,name,max_speed,mileage):
+        self.name = name
+        self.max_speed=max_speed
+        self.mileage=mileage
+
+
+
+class bus(Vechile):
+    pass
+
+school_bus = bus("school volvo", 180,12)
+
+print("Vechile.name:",school_bus.name, "speed:", school_bus.max_speed,"mileage:", school_bus.mileage)
