60352e54f8bed67360334afdbfcd08066560b11e
7382022 <amreenrida2@gmail.com>
2024-12-22 10:29:23 -0500

    Bird

---
+++ super penguin.py
@@ -0,0 +1,29 @@
+class Bird:
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisthis_(self):
+        print("Bird")
+
+    def swim (self):
+        print("swim faster")
+
+
+
+class penguin(Bird):
+
+    def __init__(self):
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisthis(self):
+        print("penguin")
+
+    def run(self):
+        print("run faster")
+
+
+peggy = penguin()
+peggy.whoisthis()
+peggy.swim()
+peggy.run()
